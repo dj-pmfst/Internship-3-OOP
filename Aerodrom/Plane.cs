ae4f2925a9fb7a53d3e5ced90dc21b3b80bfcdaf
dj-pmfst <djuric@pmfst.hr>
2025-11-19 23:08:22 +0100

    Develop planes class

---
+++ Aerodrom/Plane.cs
@@ -0,0 +1,11 @@
+﻿namespace Aerodrom
+{
+    internal class Plane
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int year { get; set; }
+        public int numberOfFlights { get; set; }
+        List<Tuple<string, int>> Seats { get; set; } = new List<Tuple<string, int>>();
+    }
+}
